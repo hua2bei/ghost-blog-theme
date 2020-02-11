b9d1fde1a81047ba8d416a5ec9c9fb3b9a51ba32
hua2bei <60107874+hua2bei@users.noreply.github.com>
2020-02-11 18:50:20 +0800

    Update and rename assets/js/config.js to 资产/ js/config.js

---
+++ 资产/ js/config.js
@@ -0,0 +1,140 @@
+/** 友情链接信息*/
+let friend = [{
+    name: “找零科技”,
+    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
+    website: “http://1zhao0.com/”，
+    github: "https://github.com/",
+    desc: “让有信用的人，手里都有现金”
+
+},{
+    na: “重庆”023花呗网
+    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
+    website: “http://023huabei.com/”, 
+    github: "https://github.com/",
+/** 友情链接信息*/
+let friend = [{
+    name: “找零科技”,
+    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
+    website: “http://1zhao0.com/”，
+    github: "https://github.com/",
+    desc: “让有信用的人，手里都有现金”
+
+},{
+    name: “重庆023花呗网”,
+    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
+    website: “http://023huabei.com/”, 
+    github: "https://github.com/",
+    desc: "不忘初心，努力做有价值的技术文章"
+
+}];
+/** 赞助人信息*/
+let sponsors = [{
+    name: "张三",
+    money: "5元"
+},{
+    name: "张三",
+    money: "5元"
+}];
+/** 关于我页面信息配置*/
+let personal = {
+    name: “花二呗”,
+    desc: “二手礼品卡回收”,
+    motto: "热爱分享，因为感恩，此生之路，我将走过；走过这一次，便再也无法重来。所有力所能及的善行，所有充盈于心的善意，我将毫不吝惜，即刻倾于。我将不再拖延，再不淡漠，只因此生之路，再也无法重来",
+    photo: [{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    }],
+    project: [{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        name: "ghost-blog-theme",
+        desc: "基于Ghost博客的一款程序员风格的主题",
+        url: "https://github.com/luokangyuan/ghost-blog-theme"
+    }]
+
+};
+/** 访问接口配置*/
+const api = new GhostContentAPI({
+    url: 'http://localhost:2370',
+    key: '3ca5b1a638723b78f35b7d4a07',
+    version: "v3"
+});
+
+
+desc: "不忘初心，努力做有价值的技术文章"
+
+}];
+/** 赞助人信息*/
+let sponsors = [{
+    name: "张三",
+    money: "5元"
+},{
+    name: "张三",
+    money: "5元"
+}];
+/** 关于我页面信息配置*/
+let personal = {
+    name: “花二呗”,
+    desc: “二手礼品卡回收”,
+    motto: "热爱分享，因为感恩，此生之路，我将走过；走过这一次，便再也无法重来。所有力所能及的善行，所有充盈于心的善意，我将毫不吝惜，即刻倾于。我将不再拖延，再不淡漠，只因此生之路，再也无法重来",
+    photo: [{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    },{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        address: "武汉 · 光谷街",
+        time: "2019年12月28日"
+    }],
+    project: [{
+        image: "http://image.luokangyuan.com/2019/08/09/09/08/08fog-foggy-lake-2649403.jpg",
+        name: "ghost-blog-theme",
+        desc: "基于Ghost博客的一款程序员风格的主题",
+        url: "https://github.com/luokangyuan/ghost-blog-theme"
+    }]
+
+};
+/** 访问接口配置*/
+const api = new GhostContentAPI({
+    url: 'http://localhost:2370',
+    key: '3ca5b1a638723b78f35b7d4a07',
+    version: "v3"
+});
+
+
